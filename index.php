31d677cd58b4f488e3d87c794955042ff60e62e8
Saleha1312 <salehabushra70@gmail.com>
2025-07-10 11:21:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <h1>THIS IS WEB APP1</h1>
+    <?php
+    echo "<h1> This app is created using azue app services</h1>";
+    ?>
+    <a href="about.php">About Us</a>
+  </body>
+</html>
